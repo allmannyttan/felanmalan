0b41960c09bd8a9d33488ac87064d93e1e0c3290
izabellalarsson <izzwii.izabella@gmail.com>
2021-11-10 13:02:44 +0100

    fix: typing for layoute component

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { ReactChild } from 'react'
 import styled from 'styled-components'
 const Wrapper = styled.div`
   margin: 1.3rem;
 `
-const Layout = ({ children }) => {
+const Layout: React.FC<{ children: ReactChild }> = ({ children }) => {
   return <Wrapper>{children}</Wrapper>
 }
 
