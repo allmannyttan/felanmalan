a1062903b7f1169697d216fe3c2b111e439445bb
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-11-15 11:03:32 +0100

    chore: button

---
+++ packages/client/src/components/Button.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+interface StyleProps {
+  disabled?: boolean
+}
+
+const StyledBtn = styled.button<StyleProps>`
+  background-color: ${(props) =>
+    props.disabled ? props.theme.colors.lightGrey : props.theme.colors.lightOrange};
+  color: ${(props) =>
+    props.disabled ? props.theme.colors.darkGrey : props.theme.colors.white};
+  border-radius: 33px;
+  border: none;
+  cursor: pointer;
+  padding: 0.8rem 0;
+  width: 90vw;
+  font-weight: 700;
+  font-size: 16px;
+  :disabled {
+    cursor: not-allowed;
+  }
+`
+
+interface Props {
+  text: string
+  disabled?: boolean
+  onClick: () => void
+  to: string
+}
+
+const Button = ({ text, disabled = false, onClick, to }: Props) => {
+  return (
+    <Link to={to}>
+      <StyledBtn onClick={onClick} disabled={disabled}>
+        {text}
+      </StyledBtn>
+    </Link>
+  )
+}
+
+export default Button
