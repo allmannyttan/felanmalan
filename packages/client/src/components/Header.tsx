73d59941817fb582607725c35dbc48f00b10523a
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-11-15 16:29:42 +0100

    chore: add arrow to header

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useLocation } from 'react-router'
+import { useLocation, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { H2 } from './Typography'
+import arrow from '../images/svg/arrow.svg'
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.paleOrange};
@@ -9,10 +10,31 @@ const Wrapper = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
+  color: ${({ theme }) => theme.colors.dark};
 `
+
+const Button = styled.button`
+  position: absolute;
+  left: 1rem;
+  top: 0.9rem;
+  background-color: inherit;
+  cursor: pointer;
+  border: none;
+`
+
 const Header = () => {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
+
   const [title, setTitle] = React.useState('Felanmälan')
+  const showArrow = [
+    '/plats',
+    '/rum',
+    '/omrade',
+    '/objekt',
+    '/komplettera',
+    '/sammanfattning',
+  ]
 
   React.useEffect(() => {
     switch (pathname) {
@@ -30,6 +52,12 @@ const Header = () => {
 
   return (
     <Wrapper>
+      {showArrow.includes(pathname) && (
+        <Button onClick={() => navigate(-1)}>
+          <img src={arrow} alt="Arrow back" />
+        </Button>
+      )}
+
       <H2>{title}</H2>
     </Wrapper>
   )
