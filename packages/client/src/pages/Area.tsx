fe03b3462f4369ed81872966837a5c57cbf94511
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-11-16 09:38:22 +0100

    chore: move progressbar logic to progressbar component

--- packages/client/src/pages/Area.tsx
+++ packages/client/src/pages/Area.tsx
@@ -1,18 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { useAtom } from 'jotai'
-import { updateProgressAtom } from '../utils/atoms'
-import { ProgressStatus, Pages } from '../utils/types'
 
 const Area = () => {
-  const [, updateProgressBar] = useAtom(updateProgressAtom)
-  useEffect(() => {
-    updateProgressBar({ page: Pages.PLACE, status: ProgressStatus.COMPLETED })
-    updateProgressBar({ page: Pages.ROOM, status: ProgressStatus.COMPLETED })
-    updateProgressBar({ page: Pages.AREA, status: ProgressStatus.DOING })
-    updateProgressBar({ page: Pages.ITEM, status: ProgressStatus.NEXT })
-  }, [])
-
   return (
     <>
       <h4>Välj ett område du vill felanmäla</h4>
