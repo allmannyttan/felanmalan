c5cf6c1f113834866612c7d004b54d6adaf51d1f
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-11-16 16:43:59 +0100

    chore: show info from atom in summary

--- packages/client/src/pages/Summary.tsx
+++ packages/client/src/pages/Summary.tsx
@@ -1,21 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useAtom } from 'jotai'
 import Button from '../components/Button'
 import { H1, BoldParagraph, Paragraph } from '../components/Typography'
+import { reportAtom } from '../utils/atoms'
 
 const TextSection = styled.div`
-  margin-bottom: 30px;
+  margin-bottom: 20px;
   width: 100%;
 `
 
 const Summary = () => {
+  const [completeErrorReport] = useAtom(reportAtom)
   return (
     <>
       <H1>Sammanfattaning av din felanmälan</H1>
-      <TextSection>
-        <BoldParagraph>Plats</BoldParagraph>
-        <Paragraph>Lägenhet</Paragraph>
-      </TextSection>
+      {completeErrorReport.complete.text && (
+        <TextSection>
+          <>
+            <BoldParagraph>Beskrivning av problemet</BoldParagraph>
+            <Paragraph>{completeErrorReport.complete.text}</Paragraph>
+          </>
+        </TextSection>
+      )}
+      {completeErrorReport.complete.image && (
+        <TextSection>
+          <>
+            <BoldParagraph>Bild</BoldParagraph>
+            <Paragraph>{completeErrorReport.complete.image?.name}</Paragraph>
+          </>
+        </TextSection>
+      )}
+      {completeErrorReport.complete.video && (
+        <TextSection>
+          <>
+            <BoldParagraph>Video</BoldParagraph>
+            <Paragraph>{completeErrorReport.complete.video?.name}</Paragraph>
+          </>
+        </TextSection>
+      )}
       <Button
         text="Skicka felanmälan"
         onClick={() => console.log('clicked')}
