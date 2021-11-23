e762db93383f686b7e44dde9ddb361fc2719335c
izabellalarsson <izzwii.izabella@gmail.com>
2021-11-23 10:41:26 +0100

    chore: change background color

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,9 +1,17 @@
 import React, { ReactChild } from 'react'
 import styled from 'styled-components'
 const Wrapper = styled.div`
-  margin: 1.3rem;
-  padding-bottom: 3rem;
+  padding: 0;
   position: relative;
+  background-color: ${({ theme }) => theme.colors.backgroundGrey};
+
+  h1 {
+    padding: 0 1.3rem;
+  }
+
+  img:first-of-type {
+    padding: 0 1.3rem;
+  }
 `
 const Layout: React.FC<{ children: ReactChild }> = ({ children }) => {
   return <Wrapper>{children}</Wrapper>
