a17be774446261d2089903c240b9fcec88ecb016
izabellalarsson <izzwii.izabella@gmail.com>
2021-11-23 11:11:31 +0100

    chore: wrap section around content

--- packages/client/src/pages/Summary.tsx
+++ packages/client/src/pages/Summary.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Button from '../components/Button'
+import Section from '../components/Section'
 import { H1, BoldParagraph, Paragraph } from '../components/Typography'
 
 const TextSection = styled.div`
@@ -11,16 +12,18 @@ const TextSection = styled.div`
 const Summary = () => {
   return (
     <>
-      <H1>Sammanfattaning av din felanmälan</H1>
-      <TextSection>
-        <BoldParagraph>Plats</BoldParagraph>
-        <Paragraph>Lägenhet</Paragraph>
-      </TextSection>
-      <Button
-        text="Skicka felanmälan"
-        onClick={() => console.log('clicked')}
-        to="/bekraftelse"
-      />
+      <H1>Sammanfattaning av felanmälan</H1>
+      <Section>
+        <TextSection>
+          <BoldParagraph>Plats</BoldParagraph>
+          <Paragraph>Lägenhet</Paragraph>
+        </TextSection>
+        <Button
+          text="Skicka felanmälan"
+          onClick={() => console.log('clicked')}
+          to="/bekraftelse"
+        />
+      </Section>
     </>
   )
 }
