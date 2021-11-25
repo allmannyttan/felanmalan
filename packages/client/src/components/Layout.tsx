8c61ef32e7f014be4d2d8d6b40e84ab1fe2ddb58
izabellalarsson <izzwii.izabella@gmail.com>
2021-11-25 10:33:18 +0100

    chore: move header inside layout component

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,16 +1,24 @@
 import React, { ReactChild } from 'react'
 import styled from 'styled-components'
+import Header from './Header'
 const Wrapper = styled.div`
   padding: 0;
   position: relative;
   background-color: ${({ theme }) => theme.colors.backgroundGrey};
+  overflow: hidden;
+  height: 100vh;
 
   .floor_plan {
     padding: 0 1.3rem;
   }
 `
 const Layout: React.FC<{ children: ReactChild }> = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>
+  return (
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
+  )
 }
 
 export default Layout
