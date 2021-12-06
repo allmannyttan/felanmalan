fa966a1802d36b69ab10fbdab3e808c8600a87f0
izabellalarsson <izzwii.izabella@gmail.com>
2021-12-06 16:01:45 +0100

    chore: get area

--- packages/server/src/routes/api.ts
+++ packages/server/src/routes/api.ts
@@ -3,6 +3,7 @@ import asyncHandler from 'express-async-handler'
 import { authMiddleware } from '@app/middleware/auth'
 import errorHandler from '@app/middleware/errorHandler'
 import { fetchApiInventory, fetchApiRooms } from '@app/services/fastapi'
+import { Area } from '@app/services/types'
 
 export const routes = (app: Application) => {
   app.get(
@@ -11,6 +12,27 @@ export const routes = (app: Application) => {
     asyncHandler(async (req: Request, res: Response) => {
       const rooms = await fetchApiRooms(req.query.rentalId as string)
       res.send(rooms)
+    }),
+    errorHandler
+  )
+
+  app.get(
+    '/area',
+    authMiddleware,
+    asyncHandler(async (req: Request, res: Response) => {
+      const inventory = await fetchApiInventory(req.query.roomId as string)
+      const reduced = inventory.reduce((filtered: Area[], option) => {
+        if (filtered.findIndex((object) => object.name === option.class.name)) {
+          filtered.push({
+            name: option.class.name,
+            description: '',
+            code: option.class.code,
+          })
+        }
+        return filtered
+      }, [])
+
+      res.send(reduced)
     }),
     errorHandler
   )
