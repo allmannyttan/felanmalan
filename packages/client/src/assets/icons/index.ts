cc1095d6f00252433d1e0717ce6ed9d4a6e34d2e
izabellalarsson <izzwii.izabella@gmail.com>
2021-12-14 10:50:53 +0100

    wip: add and structure icons

---
+++ packages/client/src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import other from './other.svg'
+import waterTap from './watertap.svg'
+import basin from './basin.svg'
+import shower from './shower.svg'
+import toilet from './toilet.svg'
+
+export { other, waterTap, basin, shower, toilet }
--- packages/client/src/components/Icon.tsx
+++ packages/client/src/components/Icon.tsx
@@ -4,27 +4,41 @@ import styled from 'styled-components'
 import apartment from '../assets/icons/apartment.svg'
 import kitchen from '../assets/icons/kitchen.svg'
 import appliances from '../assets/icons/appliances.svg'
-import waterTap from '../assets/icons/watertap.svg'
 import hallway from '../assets/icons/hallway.svg'
+import * as Svg from '../assets/icons/index'
 
-export enum Icons {
+export enum IconName {
   'Förråd',
   'Kök',
   'Vitvaror',
   'Hall',
   'Frys fullstor',
+  'Lägenhet',
+  'Övrigt',
+  'Varmvattenberedare',
+  'Tvättställ Standard',
+  'Tvättställ',
+  'Dushkabin med golv',
+  'Toalett Ifö Standard',
 }
 
-const IconVariant = {
-  [Icons['Hall']]: hallway,
-  [Icons['Kök']]: kitchen,
-  [Icons['Förråd']]: appliances,
-  [Icons['Vitvaror']]: appliances,
-  [Icons['Frys fullstor']]: appliances,
+const Icons = {
+  [IconName['Hall']]: hallway,
+  [IconName['Kök']]: kitchen,
+  [IconName['Förråd']]: appliances,
+  [IconName['Vitvaror']]: appliances,
+  [IconName['Frys fullstor']]: appliances,
+  [IconName['Lägenhet']]: apartment,
+  [IconName['Övrigt']]: Svg.other,
+  [IconName['Varmvattenberedare']]: Svg.waterTap,
+  [IconName['Tvättställ Standard']]: Svg.basin,
+  [IconName['Tvättställ']]: Svg.basin,
+  [IconName['Dushkabin med golv']]: Svg.shower,
+  [IconName['Toalett Ifö Standard']]: Svg.toilet,
 }
 
 type Props = {
-  name: Icons
+  name: IconName
   alt: string
 }
 
@@ -43,10 +57,11 @@ const Wrapper = styled.div`
 `
 
 const Icon = ({ name, alt }: Props) => {
+  console.log('hej', Icons[IconName[name as unknown as keyof typeof IconName]])
   return (
     <Wrapper>
       <SquareImg
-        src={IconVariant[Icons[name as unknown as keyof typeof Icons]]}
+        src={Icons[IconName[name as unknown as keyof typeof IconName]]}
         aria-hidden="true"
         alt={alt}
       />
