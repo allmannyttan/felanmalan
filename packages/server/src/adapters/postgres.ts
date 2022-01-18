2296f82a6276fcea91bf13867d7a28de81a74fbf
izabellalarsson <izzwii.izabella@gmail.com>
2022-01-18 15:11:30 +0100

    fix: knex config
    Co-authored-by: Emil Ahlbäck <mull@users.noreply.github.com>

--- packages/server/src/adapters/postgres.ts
+++ packages/server/src/adapters/postgres.ts
@@ -2,9 +2,5 @@ import knex from 'knex'
 
 export const db = knex({
   client: 'pg',
-  connection: {
-    uri: process.env.DATABASE_URL,
-    timezone: 'UTC',
-    dateStrings: true,
-  },
+  connection: process.env.DATABASE_URL,
 })
