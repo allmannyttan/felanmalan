917c48db883a10e330b3e8de7b099303a40321ba
Gabrielle Kamph <72319394+gabriellekamph@users.noreply.github.com>
2022-01-25 13:05:40 +0100

    feat: add startpage
    * feat: add startpage
    
    * fix: change from FlexH1 to H1

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -10,6 +10,7 @@ import ProgressBar from './components/ProgressBar'
 import { Provider as JotaiProvider } from 'jotai'
 import Layout from './components/Layout'
 import Confirmation from './pages/Confirmation'
+import Start from './pages/Start'
 
 const App = () => {
   const { pathname } = useLocation()
@@ -29,6 +30,7 @@ const App = () => {
           <>
             {showProgressBar.includes(pathname) && <ProgressBar />}
             <Routes>
+              <Route path="/" element={<Start />} />
               <Route path="/plats" element={<Place />} />
               <Route path="/rum" element={<Room />} />
               <Route path="/omrade" element={<Area />} />
---
+++ packages/client/src/pages/Start.tsx
@@ -0,0 +1,57 @@
+import Section from '../components/Section'
+import { H1, Paragraph } from '../components/Typography'
+import styled from 'styled-components'
+import Button from '../components/Button'
+import active from '../images/svg/active.svg'
+import { devices } from '../utils/devices'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  overflow: hidden;
+  height: 75vh;
+  justify-content: center;
+  padding: 0 4rem;
+`
+
+const Image = styled.img`
+  margin: 1rem auto;
+`
+
+const ButtonWrapper = styled.div`
+  max-width: 840px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+
+  @media only screen and (${devices.tablet}) {
+    bottom: 2rem;
+    position: absolute;
+  }
+`
+
+const Start = () => {
+  return (
+    <Section>
+      <Container>
+        <Image src={active} alt="Active" />
+        <H1>Du har inga aktiva felanmälningar</H1>
+        <Paragraph center={true}>
+          När du har gjort en felanmälan kommer den att hamna i en lista här.
+        </Paragraph>
+      </Container>
+      <ButtonWrapper>
+        <Button
+          text="Skapa felanmälan"
+          to="/plats"
+          onClick={() => console.log('create new felanmälan')}
+        />
+      </ButtonWrapper>
+    </Section>
+  )
+}
+
+export default Start
