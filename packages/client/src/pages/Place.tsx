b5133872626d681866042d44d8615d0b239bf3d5
izabellalarsson <izzwii.izabella@gmail.com>
2022-02-09 16:16:54 +0100

    wip: layout "show common error reports"

--- packages/client/src/pages/Place.tsx
+++ packages/client/src/pages/Place.tsx
@@ -4,6 +4,7 @@ import NextStepCard from '../components/NextStepCard'
 import Section from '../components/Section'
 import { H1 } from '../components/Typography'
 import Elements from '../shared-elements'
+import { FlexToStart } from '../shared-elements/layout'
 import { userAtom } from '../utils/atoms'
 
 const Place = () => {
@@ -14,10 +15,32 @@ const Place = () => {
   }, [])
   return (
     <>
-      <H1>Välj en plats</H1>
-      <Section>
+      <FlexToStart>
+        <H1>Välj en plats</H1>
+      </FlexToStart>
+      <Section bg="inherit">
         <Elements.Layout.Ul>
           <li>
+            <NextStepCard
+              bg="inherit"
+              title="Lägenhet"
+              subtitle="Kök, badrum, sovrum"
+              sendTo="rum"
+            />
+          </li>
+        </Elements.Layout.Ul>
+      </Section>
+      <Section>
+        <FlexToStart>
+          <H1>Vanliga felanmälnninngar</H1>
+        </FlexToStart>
+        <Elements.Layout.Ul>
+          <li>
+            <NextStepCard title="Lägenhet" subtitle="Kök, badrum, sovrum" sendTo="rum" />
+            <NextStepCard title="Lägenhet" subtitle="Kök, badrum, sovrum" sendTo="rum" />
+            <NextStepCard title="Lägenhet" subtitle="Kök, badrum, sovrum" sendTo="rum" />
+            <NextStepCard title="Lägenhet" subtitle="Kök, badrum, sovrum" sendTo="rum" />
+            <NextStepCard title="Lägenhet" subtitle="Kök, badrum, sovrum" sendTo="rum" />
             <NextStepCard title="Lägenhet" subtitle="Kök, badrum, sovrum" sendTo="rum" />
           </li>
         </Elements.Layout.Ul>
